0a5857785e130efc1726d6eae1230325115772f4
urielrc18 <85496327+urielrc18@users.noreply.github.com>
2023-06-20 02:39:14 -0500

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,3 +4,8 @@
 docker pull php:latest
 docker pull nginx:latest
 docker pull mysql:latest
+
+export AWS_ACCESS_KEY_ID=AKIAZPZC3BZMDQIG5EVA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-west-2
+aws configure
