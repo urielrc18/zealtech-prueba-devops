c45b86610cbf366ffcaa278d7cd5702625db8b53
urielrc18 <85496327+urielrc18@users.noreply.github.com>
2023-06-20 02:55:48 -0500

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Pull the latest versions of PHP, Nginx, and MySQL images from Docker Hub
@@ -11,4 +12,7 @@ sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt update && sudo apt install terraform
 terraform --version 
+cd terraform
+terraform init
+terraform plan
 pwd
